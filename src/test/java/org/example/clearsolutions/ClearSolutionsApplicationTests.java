c33b10017ff85e5bde2cb91732c93cad4865f4cb
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-05-01 20:07:39 +0300

    Added infrastructure

---
+++ src/test/java/org/example/clearsolutions/ClearSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clearsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
